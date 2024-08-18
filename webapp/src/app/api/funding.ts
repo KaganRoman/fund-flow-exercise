aa4e635af8bbbfdd5d13ffe5184f85eb3b8e7ba0
Roman Kagan <don.eric@gmail.com>
2024-08-18 03:21:41 +0300

    implement web side

--- webapp/src/app/api/funding.ts
+++ webapp/src/app/api/funding.ts
@@ -1,9 +1,29 @@
 "use client";
 
 import { ChainAddress } from '@/app/types/chainAddress';
-import { FundingResData } from '@/app/types/fundingRecord';
+import { FundingResData, FundingFlowRecord } from '@/app/types/fundingRecord';
 
 
-export const getFundingGraph = async (source: ChainAddress): Promise<FundingResData> => {
+export const getFundingGraph = async (): Promise<FundingResData> => {
+    const mapEdgeToFlowRecord = (edge): FundingFlowRecord => {
+        return {
+            source: {
+                address: edge.source.address,
+                chainId: edge.source.chain_id,
+                type: edge.source.type,
+                name: edge.source.name,
+            } as ChainAddress,
+            dest: {
+                address: edge.dest.address,
+                chainId: edge.dest.chain_id,
+                type: edge.dest.type,
+                name: edge.dest.name,
+            } as ChainAddress
+        } as FundingFlowRecord;
+    }
 
+    const BASE_URL = 'http://0.0.0.0:8000/api/v1'  // todo: take it from config
+    const response = await fetch(`${BASE_URL}/funding/graph`);
+    const data = await response.json();
+    return { edges: data.edges.map(mapEdgeToFlowRecord) } as FundingResData
 };
--- webapp/src/app/dashboard/page.tsx
+++ webapp/src/app/dashboard/page.tsx
@@ -1,13 +1,19 @@
 'use client';
+import FlowChart from '@/app/components/charts/FlowChart';
+import { Spinner } from '@/app/components/spinner/Spinner';
+import { useFundsFlow } from '@/app/dashboard/hooks/useFundsFlow';
 
-import { AddressType, ChainAddress, ChainIds } from '@/app/types/chainAddress';
-
-const sourceChainAddress: ChainAddress = {
-    address: '0x39cd23328b5ba304ae70bb0c1866e224f727f962',
-    chainId: ChainIds.EthereumMainnet,
-    type: AddressType.EOA,
-};
 
 export default function Dashboard() {
+    const { isLoading, error, nodes, edges } = useFundsFlow();
 
+    if (isLoading) {
+        return <Spinner />;
+    }
+
+    if (error) {
+        return <span>Error: {error}</span>;
+    }
+
+    return ( <FlowChart nodes={nodes} edges={edges} />);
 }
